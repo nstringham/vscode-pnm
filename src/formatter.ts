1027a3d2ea21ec212b092816126dfc12d4f7d27a
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2025-11-05 22:12:13 -0500

    refactor: ♻️ move formatter code into its own file

---
+++ src/formatter.ts
@@ -0,0 +1,33 @@
+import { DocumentFormattingEditProvider, Position, Range, TextEdit } from "vscode";
+
+export const formatter: DocumentFormattingEditProvider = {
+  provideDocumentFormattingEdits(document) {
+    const edits: TextEdit[] = [];
+
+    for (let i = 0; i < document.lineCount; i++) {
+      const line = document.lineAt(i);
+
+      const leadingWhitespace = /^\s+/d.exec(line.text);
+      if (leadingWhitespace != null) {
+        const [start, end] = leadingWhitespace.indices![0];
+        const range = new Range(i, start, i, end);
+        edits.push(TextEdit.delete(range));
+      }
+
+      const trailingWhitespace = /\s+$/d.exec(line.text);
+      if (trailingWhitespace != null) {
+        const [start, end] = trailingWhitespace.indices![0];
+        const range = new Range(i, start, i, end);
+        edits.push(TextEdit.delete(range));
+      }
+
+      const commentWithoutSpace = /^[^#]*#[^ ]/.exec(line.text);
+      if (commentWithoutSpace != null) {
+        const position = new Position(i, commentWithoutSpace[0].length - 1);
+        edits.push(TextEdit.insert(position, " "));
+      }
+    }
+
+    return edits;
+  },
+};
